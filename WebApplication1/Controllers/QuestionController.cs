d088930ddf76cb9b89873daa509a25e316c07b54
N1gaz <58673572+N1gaz@users.noreply.github.com>
2021-03-16 10:46:54 +0300

    Окончание работы над документацией

--- WebApplication1/Controllers/QuestionController.cs
+++ WebApplication1/Controllers/QuestionController.cs
@@ -9,13 +9,23 @@ using System.Threading.Tasks;
 
 namespace SurveysWebApi.Controllers
 {
+    /// <summary>
+    /// API controller for acess to application
+    /// </summary>
     [Route("")]
     [ApiController]
     public class QuestionController : ControllerBase
     {
         private IMediator _mediator;
+        /// <summary>
+        /// Mediator property
+        /// </summary>
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-
+        /// <summary>
+        /// This method tries to return question by HttpGet. Returns 404 when we have invalid id.
+        /// </summary>
+        /// <param name="id">question id</param>
+        /// <returns>Question object in Json or 404</returns>
         [HttpGet("question/{id:int}")]
         public async Task<IActionResult> GetQuestion(int id)
         {
@@ -29,6 +39,13 @@ namespace SurveysWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// This method tries to add Answer result in database application. Returns 400 if comand is invalid by
+        /// AnswerId or
+        /// InterviewId
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns>Next question Id or 400</returns>
         [HttpPost("answer")]
         public async Task<IActionResult> PostAnswerResult(CreateResultCommand command)
         {
