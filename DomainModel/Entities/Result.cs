2bcf6c977bff7ca6c06a222dbccb3c516047864b
N1gaz <58673572+N1gaz@users.noreply.github.com>
2021-03-16 10:18:43 +0300

    Модель полностью задокументирована

--- DomainModel/Entities/Result.cs
+++ DomainModel/Entities/Result.cs
@@ -1,20 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace DomainModel.Entities
-{ 
+{
     ///<summary>
     ///This class contains database entity.
-    ///It was generated using Scaffold-DbContext
+    ///It was generated using Scaffold-DbContext.
+    ///Both fields make up the primary key.
     ///</summary>
-public partial class Result
+    public partial class Result
     {
+        /// <summary>
+        /// Reference to Answers table.
+        /// </summary>
         [Required]
         public int AnswerId { get; set; }
-
+        /// <summary>
+        /// Reference to Interviews table
+        /// </summary>
         [Required]
         public int InterviewId { get; set; }
-
+        /// <summary>
+        /// Reference to Answer
+        /// </summary>
         public virtual Answer Answer { get; set; }
+        /// <summary>
+        /// Reference to Interview
+        /// </summary>
         public virtual Interview Interview { get; set; }
     }
 }
